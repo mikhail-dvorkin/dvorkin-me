840c9c54eea7acbe81d183ab842bc0c470b24c1a
Mikhail Dvorkin <mikhail.dvorkin@gmail.com>
2020-05-16 13:45:04 +0300

    Change spaces to tabs

--- src/Application.kt
+++ src/Application.kt
@@ -7,9 +7,9 @@ import io.ktor.routing.*
 fun main(args: Array<String>) = io.ktor.server.netty.EngineMain.main(args)
 
 fun Application.module() {
-    routing {
-        get("/") {
-            call.respondText("Hello World!")
-        }
-    }
+	routing {
+		get("/") {
+			call.respondText("Hello World!")
+		}
+	}
 }
