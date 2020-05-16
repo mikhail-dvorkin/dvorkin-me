b45058b9ddd39c486ca862f9561d6d4c34f62e7c
Mikhail Dvorkin <mikhail.dvorkin@gmail.com>
2020-05-16 23:45:47 +0300

    Add testRoot

--- test/ApplicationTest.kt
+++ test/ApplicationTest.kt
@@ -1,10 +1,22 @@
 package me.dvorkin
 
 import kotlin.test.*
+import io.ktor.http.*
+import io.ktor.server.testing.*
 
 class ApplicationTest {
     @Test
     fun foo() {
         assertEquals(1, 1)
+    }
+
+    @Test
+    fun testRoot() {
+        withTestApplication({ module() }) {
+            handleRequest(HttpMethod.Get, "/").apply {
+                assertEquals(HttpStatusCode.OK, response.status())
+                assertEquals("Hello World!", response.content)
+            }
+        }
     }
 }
